3aa5e4068985ef6a788ee813a701770825821e65
wahaha216 <wahaha216@outlook.com>
2024-08-17 11:33:23 +0800

    初版

--- readme.md
+++ readme.md
@@ -2,4 +2,30 @@
 
 [![npm](https://img.shields.io/npm/v/@wahaha216/koishi-plugin-webhook-trigger?style=flat-square)](https://www.npmjs.com/package/@wahaha216/koishi-plugin-webhook-trigger)
 
-处理webhook请求，并使用handlebars格式化
+处理webhook请求，并使用[handlebars](https://handlebarsjs.com/guide/)格式化
+
+插件已扩展一个用法 `if_equals` ，判断某个键的值是否与指定值相等，使用示例：
+
+接收的数据
+```json
+{
+  "test": "test",
+  "a": "aaaaaaaa",
+  "type": "add",
+  "b": "bbbbbb"
+}
+```
+格式化消息体
+```tex
+这是测试消息，{{ test }}
+{{#if_equals type 'add'}}
+{{a}}
+{{else}}
+{{b}}
+{{/if_equals}}
+```
+结果
+```tex
+这是测试消息，test
+aaaaaaaa
+```
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,110 @@
-import { Context, Schema } from 'koishi'
+import { Context, Schema } from "koishi";
+import {} from "@koishijs/plugin-server";
+import Handlebars from "handlebars";
 
-export const name = 'webhook-trigger'
+export const name = "webhook-trigger";
 
-export interface Config {}
+export const inject = {
+  required: ["server", "logger"],
+};
 
-export const Config: Schema<Config> = Schema.object({})
+interface Listenners {
+  url: string;
+  method: "get" | "post";
+  headers: Record<string, string | null>;
+  pushChannelIds: string[];
+  pushPrivateIds: string[];
+  msg: string;
+}
+export interface Config {
+  defaultPrefix: boolean;
+  listeners: Listenners[];
+}
 
-export function apply(ctx: Context) {
+export const Config: Schema<Config> = Schema.intersect([
+  Schema.object({
+    defaultPrefix: Schema.boolean().default(true),
+  }),
+  Schema.object({
+    listeners: Schema.array(
+      Schema.object({
+        url: Schema.string().required(),
+        method: Schema.union(["get", "post"]).default("post"),
+        headers: Schema.dict(Schema.string()).role("table"),
+        pushChannelIds: Schema.array(String),
+        pushPrivateIds: Schema.array(String),
+        msg: Schema.string().role("textarea"),
+      })
+    ),
+  }),
+]).i18n({
+  "zh-CN": require("./locales/zh-CN")._config,
+  "en-US": require("./locales/en-US")._config,
+});
+
+export function apply(ctx: Context, config: Config) {
   // write your plugin here
+  const logger = ctx.logger("wahaha216-webhook");
+
+  logger.info("Register Handlebars Helper: if_equals");
+  Handlebars.registerHelper("if_equals", function (p, q, options) {
+    return p === q ? options.fn(this) : options.inverse(this);
+  });
+
+  for (const ls of config.listeners) {
+    const prefix = config.defaultPrefix ? `/webhook` : "";
+    const url = ls.url.startsWith("/") ? ls.url : `/${ls.url}`;
+    const fullUrl = `${prefix}${url}`;
+    if (ls.method === "get") {
+      ctx.server.get(
+        fullUrl,
+        (content, next) => {
+          for (let httpheader in ls.headers) {
+            // 检查头，如果不相等则返回400
+            if (content.header[httpheader] != ls.headers[httpheader])
+              return (content.status = 400);
+          }
+          next();
+        },
+        async (content) => {
+          logger.info(`get: ${fullUrl}`);
+          const template = Handlebars.compile(ls.msg);
+          const result = template(content.request.query);
+          for (const bot of ctx.bots) {
+            for (const channelId of ls.pushChannelIds) {
+              await bot.sendMessage(channelId, result);
+            }
+            for (const privateId of ls.pushPrivateIds) {
+              await bot.sendPrivateMessage(privateId, result);
+            }
+          }
+        }
+      );
+    } else {
+      ctx.server.post(
+        fullUrl,
+        (content, next) => {
+          for (let httpheader in ls.headers) {
+            // 检查头，如果不相等则返回400
+            if (content.header[httpheader] != ls.headers[httpheader])
+              return (content.status = 400);
+          }
+          next();
+        },
+        async (content) => {
+          logger.info(`post: ${fullUrl}`);
+          const template = Handlebars.compile(ls.msg);
+          const result = template(content.request.body);
+          for (const bot of ctx.bots) {
+            for (const channelId of ls.pushChannelIds) {
+              await bot.sendMessage(channelId, result);
+            }
+            for (const privateId of ls.pushPrivateIds) {
+              await bot.sendPrivateMessage(privateId, result);
+            }
+          }
+        }
+      );
+    }
+  }
 }
